88a686388ee2a8ac0e3d417e72a397ac2e935013
EdwardJhosep <1454668@senati.pe>
2023-09-21 17:52:25 -0500

    calculadora final era en blanco

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using App1.Services;
-using App1.Views;
-using System;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,13 +6,11 @@ namespace App1
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
